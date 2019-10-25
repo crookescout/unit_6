c540eeeb221bae57bbb7fbf5ec190e9d68b27d94
scout <scout.crooke@ssfs.org>
2019-10-25 09:35:35 -0400

    Initial commit

---
+++ daily_exercises.py
@@ -0,0 +1,6 @@
+# Scout Crooke, 11/25/19, this prgram works on unit 6 daily exercises
+
+names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen", "Jackie",
+         "Kurt", "Linda"]
+
+print(names[3:5])
