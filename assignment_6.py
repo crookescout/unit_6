0ac5bed72afb083b80b0ba6232e365e6891e327d
scout <scout.crooke@ssfs.org>
2019-11-05 09:01:03 -0500

    Worked on unit six assignment option 2

---
+++ assignment_6.py
@@ -0,0 +1,27 @@
+# Scout Crooke, 11/5/19, This program
+
+
+def list_nums():
+    max_num = int(input("What is the maximum number you would like to use?"))
+    num_list = []
+    for x in range(2, max_num + 1):
+        num_list.append(x)
+    return num_list
+
+
+def prime_list(num_list):
+    primes = []
+    num = num_list[0]
+    primes.append(num_list[0])
+    for x in num_list:
+        if x % num[0] == 0:
+            num_list.remove(x)
+    return prime_list(num_list)
+
+
+def main():
+    num_list = list_nums()
+    print("The list of prime numbers is:", prime_list(num_list))
+
+
+main()
